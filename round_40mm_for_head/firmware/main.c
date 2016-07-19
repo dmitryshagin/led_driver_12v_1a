5387b41a0d85c6fcded79ad191ff357acf6e5d0a
Dmitry Shagin <dmitry.shagin@gmail.com>
2016-07-19 22:35:07 +0300

    version 1.0

--- round_40mm_for_head/firmware/main.c
+++ round_40mm_for_head/firmware/main.c
@@ -3,6 +3,13 @@
 #include <avr/interrupt.h>
 #include <avr/sleep.h> 
 
+#define MODE_OFF			0
+#define MODE_IDLE			1
+#define MODE_OVERTEMP		2
+#define MODE_LOW_VOLTAGE	3
+#define MODE_LOW			4
+#define MODE_HALF			5
+#define MODE_FULL			6
 
 #define BLUE_OFF 			(PORTA |=  (1<<PA4))
 #define BLUE_ON 			(PORTA &= ~(1<<PA4))
@@ -17,20 +24,16 @@
 #define DRIVER_ON  			(PORTB |=  (1<<PB0))
 #define DRIVER_OFF 			(PORTB &= ~(1<<PB0))
 
-// #define LED_ON  			(PORTA |=  (1<<PA7))
-// #define LED_OFF 			(PORTA &= ~(1<<PA7))
 
+volatile uint8_t power_mode, unlock_stage, d_seconds, adc_channel;
+volatile uint16_t val;
+volatile uint32_t seconds;
 
-volatile uint8_t need_to_sleep, current_mode, power_mode;
-volatile uint32_t seconds, d_seconds;
-volatile uint16_t val=1024;
-
-// ISR(ADC_vect){
-// 	uint8_t theLowADC = ADCL;
-// 	val = ADCH<<8 | theLowADC;
-// 	ADCSRA |= 1<<ADSC;	//start conversion
-// // TODO - round-robin between voltage and temp
-// }
+ISR(ADC_vect){
+	uint8_t theLowADC = ADCL;
+	val = ADCH<<8 | theLowADC;
+	ADCSRA |= 1<<ADSC;	//start conversion
+}
 
 ISR(PCINT1_vect){
 	read_button();
@@ -38,93 +41,107 @@ ISR(PCINT1_vect){
 
 ISR(TIM1_OVF_vect){
 	d_seconds+=1;
-	if(d_seconds==9){
+	if(d_seconds==10){
 		seconds+=1;
 		d_seconds=0;
-		PORTA ^= (1<<PA4);
 	}
 	TCNT1 = 53035; //~=10Hz
 }
 
+void all_off(){
+	stop_timer();
+	unlock_stage = 0;
+	seconds = 0;
+	d_seconds = 0;
+	RED_OFF;GREEN_OFF;BLUE_OFF;
+	power_mode = MODE_OFF;
+	apply_power();
+	DRIVER_OFF;
+}
+
+void start_timer(){
+	TCCR1A = 0;
+	TCNT1 = 53035; 		 //~=10Hz
+	TCCR1B = (1<<CS11); 
+	TIMSK1 = (1<<TOIE1); //timer1 interrupt enabled
+	ADCSRA |= (1<<ADSC); //start ADC conversion
+}
+
+void stop_timer(){
+	TIMSK1 = 0;
+	TCCR1B = 0;
+}
 
 int apply_power(void){
-	if(power_mode==255){power_mode=0;}
-	if(power_mode>4){power_mode=4;}
+	if(unlock_stage>0){
+		GREEN_ON;
+	}
+	if(unlock_stage==3){ //unlock finished
+		power_mode = MODE_IDLE;
+		unlock_stage = 0;
+	}
+	if(power_mode==255){power_mode=MODE_IDLE;}
+	if(power_mode>MODE_FULL){power_mode=MODE_FULL;}
 	
-	if(power_mode==0){ 
+	if(power_mode==MODE_OFF){ 
 		DRIVER_OFF; 
 		OCR0B=0;
-		TIMSK1 = 0;
-		TCCR1B = 0;
-	}
-	else{
+	}else{
 		DRIVER_ON; 
 		set_sleep_mode(SLEEP_MODE_IDLE); 
 		sleep_disable();
-		TCCR1A = 0;
-		TCNT1 = 53035; //~=10Hz
-		TCCR1B = (1<<CS11); 
-		TIMSK1 = (1<<TOIE1); //timer1 interrupt enabled
+		start_timer();
 	}
-	// if(power_mode==1){DRIVER_ON;  OCR0B=16;}
-	// if(power_mode==2){DRIVER_ON;  OCR0B=64;}
-	// if(power_mode==3){DRIVER_ON;  OCR0B=127;}
-	// if(power_mode==4){DRIVER_ON;  OCR0B=255;}
-	if(power_mode==1){OCR0B=1;}
-	if(power_mode==2){OCR0B=2;}
-	if(power_mode==3){OCR0B=3;}
-	if(power_mode==4){OCR0B=4;}
+	seconds=0;
+	if(power_mode==MODE_IDLE)		{OCR0B=0;}
+	if(power_mode==MODE_LOW_VOLTAGE){OCR0B=16;}
+	if(power_mode==MODE_OVERTEMP)	{OCR0B=8;}
+	if(power_mode==MODE_LOW)		{OCR0B=64;}
+	if(power_mode==MODE_HALF)		{OCR0B=127;}
+	if(power_mode==MODE_FULL)		{OCR0B=255;}
 	return 0;
 }
 
 void read_button(void){
-	// if(power_mode>0){
-	// 	// if(val<755){
-	// 	if(val<100){
-	// 		current_mode = 1- current_mode;
-	// 		if(current_mode){
-	// 			RED_ON;
-	// 		}else{
-	// 			RED_OFF;
-	// 		}	
-	// 		// TODO - перевесить светодиоды на PWM и плавно моргать
-	// 		power_mode = 1;
-	// 		_delay_ms(1200);
-	// 		GREEN_OFF;
-	// 		BLUE_OFF;
-	// 	}else if(val<793){
-	// 		RED_ON;
-	// 		GREEN_OFF;
-	// 		BLUE_OFF;
-	// 	}else if(val<872){
-	// 		RED_OFF;
-	// 		GREEN_OFF;
-	// 		BLUE_ON;
-	// 	}else{
-	// 		RED_OFF;
-	// 		BLUE_OFF;
-	// 		GREEN_ON;	
-	// 	}
-	// }else{
-	// 	RED_OFF;
-	// 	BLUE_OFF;
-	// 	GREEN_OFF;
-	// 	//TODO - вырубать драйвер
-	// }	
+	uint32_t lock_timer=0;
+	start_timer();
 	if(LEFT_PRESSED){
 		RED_OFF;GREEN_OFF;BLUE_ON;
-		power_mode-=1;
+		if(power_mode==MODE_OFF){
+			if(unlock_stage==0 || unlock_stage==2){
+				unlock_stage+=1;
+			}
+		}else{
+			if(power_mode>MODE_LOW){
+				power_mode-=1;
+			}else{
+				power_mode=1;
+			}	
+		}	
 		apply_power();
-		while(LEFT_PRESSED){};
+		while(LEFT_PRESSED){lock_timer+=1;if(lock_timer==100000){all_off();}};
 		BLUE_OFF;
+		_delay_ms(200);
 	}
 	if(RIGHT_PRESSED){
 		RED_OFF;GREEN_OFF;BLUE_ON;
-		power_mode+=1;
+		if(power_mode==MODE_OFF){
+			if(unlock_stage==1){
+				unlock_stage+=1;
+			}
+		}else{	
+			if(power_mode>=MODE_LOW){ // проскакиваем аварийные режимы
+				power_mode+=1;
+			}else{
+				power_mode=MODE_LOW;
+			}	
+		}	
 		apply_power();
 		while(RIGHT_PRESSED){};
 		BLUE_OFF;
+		_delay_ms(200);
 	}
+
 }
 
 int main(void){
@@ -137,56 +154,25 @@ int main(void){
 	// PB1 - hall1, PB2 - hall2
 
 	DDRA  = 0b11111110;
-	// PORTA = 0xFF;
 	DDRB  = 0b0001;
 	PORTB  = 0b0111;
 
-	BLUE_OFF;
-	GREEN_OFF;
+	all_off();
+	
+	RED_ON;
+	_delay_ms(500);
 	RED_OFF;
 
-	// DRIVER_ON;
-	// LED_OFF;
-	need_to_sleep = 1;
-	power_mode=0;
-	BLUE_OFF;
-	RED_OFF;
-	GREEN_OFF;
-	DRIVER_OFF;
-// LED_OFF;
-// _delay_ms(1500);
-// LED_ON;
-// _delay_ms(1500);
-RED_ON;
-_delay_ms(500);
-RED_OFF;
 	TCCR0A = (1 << COM0B1) | (1 << WGM00);// | (1 << WGM01);  		// PWM mode
 	TCCR0B = (1 << CS01);							// clock source = CLK/8, start PWM
 
 	PCMSK1 = (1<<PCINT9) | (1<<PCINT10);
-
-	// TCNT1H = 0xCF;
-	// TCNT1L = 0x2B; //~=10Hz
-
-	// GIFR = (1<<PCIF1); 
 	GIMSK = (1<<PCIE1);//interrupt on pin change0
-	// OCR0B = 0x00;
-// LED_ON;
 
-	// TCCR1A = (1 << COM0B1) | (1 << WGM00);  		// PWM mode
-	// TCCR1B = (1 << CS01);							// clock source = CLK/8, start PWM
-	// OCR1A = 0xFFFF;
-	// OCR1B = 0xFFFF;
-
-	// ADMUX = 0b10100010; // int reference and temp sensor
 	ADMUX = 0b10000000; // ADC0 - int
+	ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0) | (1<<ADIE);	// enable ADC on 1mhz/16 with interrupt
+	ADCSRA |= (1<<ADSC);							//start conversion
 
-	// ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0) | (1<<ADIE);	// enable ADC on 1mhz/16 with interrupt
-	// ADCSRA |= (1<<ADSC);							//start conversion
-	// //временно вырубил АЦП и забил значение жестко
-
-
-	uint8_t initial = PINB;
 	sei();
 
 	while(1){
@@ -196,53 +182,84 @@ RED_OFF;
 		// 11V = 872
 		// 12V = 951
 		// 12.6V = 999
-		if(power_mode==0){
+		if(power_mode==MODE_OFF && unlock_stage==0){
+			seconds = 0;
+			d_seconds = 0;
 			set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 			sleep_enable();
 			sleep_bod_disable();
 			sleep_cpu();
 			sleep_disable();
 		}	
-		
-		// while(LEFT_PRESSED || RIGHT_PRESSED){}//UGLY debounce :)
-		
+		if(seconds>1 && power_mode==MODE_OFF){
+			all_off();
+		}
+
+		if(d_seconds==0){
+			adc_channel = 0;
+			ADMUX = 0b10000000; // ADC0 - int
+		}	
+		if(d_seconds==5){	
+			adc_channel = 1;
+			ADMUX = 0b10100010; // int reference and temp sensor
+		}
+
+		if(seconds%2==0){
+			if(power_mode==MODE_LOW_VOLTAGE){
+				RED_ON;GREEN_OFF;BLUE_OFF;
+			}
+			if(power_mode==MODE_OVERTEMP){
+				RED_ON;GREEN_ON;BLUE_OFF;
+			}
+		}else{
+			if(power_mode==MODE_OVERTEMP||power_mode==MODE_LOW_VOLTAGE){
+				RED_OFF;GREEN_OFF;BLUE_OFF;
+			}
+		}
 
 
- 	// 	if(val>999){
-		// 	GREEN_ON;
-		// }else{
-		// 	GREEN_OFF;	
-		// }
-		// BLUE_ON;
-		// _delay_ms(5);
-		// BLUE_OFF;
-		// _delay_ms(250);
+		// if((seconds%5==0) && ADMUX == 0b10000000){ //measure voltage in active mode
+		if(adc_channel ==0 && d_seconds == 4){ //measure voltage in active mode
+			if(val<714){
+				all_off();//completely discharged. turning off
+			}
+			if(power_mode!=MODE_OVERTEMP){ //show voltage always, except overtemp
+				if(val<755){
+					if(power_mode>=MODE_LOW){ // усвловие нужно, чтобы не задалбывать функцию - в ней будут логи
+						power_mode = MODE_LOW_VOLTAGE;//low voltage
+						apply_power();
+					}	
+				}else if(val<793){
+					RED_ON;
+					GREEN_OFF;
+					BLUE_OFF;
+				}else if(val<872){
+					RED_OFF;
+					GREEN_OFF;
+					BLUE_ON;
+				}else{
+					RED_OFF;
+					BLUE_OFF;
+					GREEN_ON;	
+				}
+			}	
+		}
 
-		// sei();
-		// if(need_to_sleep){
-			// sleep_mode();
-		// }
-		// if(LEFT_PRESSED){GREEN_OFF;BLUE_ON;}
-		// if(RIGHT_PRESSED){BLUE_OFF;GREEN_ON;}
-		
+		if(adc_channel ==1 && d_seconds == 8){ //measure temp in active mode
+			if(val>330 && power_mode>=MODE_LOW ){// усвловие нужно, чтобы не задалбывать функцию - в ней будут логи
+				power_mode = MODE_OVERTEMP;
+				apply_power(); //overtemp. reduce current cunsumption
+			}
+		}
 
-		// if(LEFT_PRESSED){
-		// 	OCR0B=16;
-		// 	BLUE_ON;
-		// }else{
-		// 	BLUE_OFF;
-		// 	GREEN_OFF;
-		// }
-		// if(RIGHT_PRESSED){
-		// 	OCR0B = 0;
-		// 	GREEN_ON;
-		// }else{
-		// 	GREEN_OFF;
-		// 	BLUE_OFF;
-		// }
-		
+		if(power_mode==MODE_IDLE && seconds>=7200){ //выключение через два часа из режима простоя
+			all_off();
+		}
 
-
-		// if(RIGHT_PRESSED){RED_ON;}
+		if(seconds==0 && d_seconds<5 && power_mode>=MODE_LOW){
+			GIMSK = 0;//вырубим прерывания на первые 500мс, чтобы не кнопать кнопкой сильно часто
+		}else{
+			GIMSK = (1<<PCIE1);//interrupt on pin change0
+		}
 	}	
 }
