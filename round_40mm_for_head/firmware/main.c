86c26b44eb908594ef6a66a6010eef4429a97bf9
Dmitry Shagin <dmitry.shagin@gmail.com>
2016-07-25 20:41:15 +0300

    added eeprom storage, temporary disabled everything for testing

--- round_40mm_for_head/firmware/main.c
+++ round_40mm_for_head/firmware/main.c
@@ -2,6 +2,7 @@
 #include <avr/delay.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h> 
+#include <avr/eeprom.h>
 
 #define MODE_OFF			0
 #define MODE_IDLE			1
@@ -25,9 +26,28 @@
 #define DRIVER_OFF 			(PORTB &= ~(1<<PB0))
 
 
-volatile uint8_t power_mode, unlock_stage, d_seconds, adc_channel;
+volatile uint8_t power_mode, prev_power_mode, unlock_stage, d_seconds, adc_channel;
 volatile uint16_t val;
 volatile uint32_t seconds;
+
+typedef struct SYSTEM_CONFIG{
+	uint32_t seconds_idle;
+	uint32_t seconds_overtemp;
+	uint32_t seconds_low_voltage;
+	uint32_t seconds_low;
+	uint32_t seconds_med;
+	uint32_t seconds_high;
+	uint16_t times_idle;
+	uint16_t times_overtemp;
+	uint16_t times_low_voltage;
+	uint16_t times_low;
+	uint16_t times_med;
+	uint16_t times_high;
+} systemConfig_t;
+
+struct SYSTEM_CONFIG nv_system_config EEMEM = {0,0,0,0,0,0, 0,0,0,0,0,0};
+struct SYSTEM_CONFIG system_config;
+// struct SYSTEM_CONFIG stored_system_config;
 
 ISR(ADC_vect){
 	uint8_t theLowADC = ADCL;
@@ -48,6 +68,7 @@ ISR(TIM1_OVF_vect){
 	TCNT1 = 53035; //~=10Hz
 }
 
+
 void all_off(){
 	stop_timer();
 	unlock_stage = 0;
@@ -57,6 +78,7 @@ void all_off(){
 	power_mode = MODE_OFF;
 	apply_power();
 	DRIVER_OFF;
+	OCR0B=0;
 }
 
 void start_timer(){
@@ -92,13 +114,28 @@ int apply_power(void){
 		sleep_disable();
 		start_timer();
 	}
-	seconds=0;
-	if(power_mode==MODE_IDLE)		{OCR0B=0;}
-	if(power_mode==MODE_LOW_VOLTAGE){OCR0B=16;}
-	if(power_mode==MODE_OVERTEMP)	{OCR0B=8;}
-	if(power_mode==MODE_LOW)		{OCR0B=64;}
+	if(power_mode==MODE_IDLE)		{OCR0B=0;  }
+	if(power_mode==MODE_LOW_VOLTAGE){OCR0B=16; }
+	if(power_mode==MODE_OVERTEMP)	{OCR0B=8;  }
+	if(power_mode==MODE_LOW)		{OCR0B=64; }
 	if(power_mode==MODE_HALF)		{OCR0B=127;}
 	if(power_mode==MODE_FULL)		{OCR0B=255;}
+
+	if(prev_power_mode!=power_mode){
+		if(prev_power_mode==MODE_IDLE)		 {system_config.seconds_idle+=seconds;        system_config.times_idle+=1;       }
+		if(prev_power_mode==MODE_LOW_VOLTAGE){system_config.seconds_low_voltage+=seconds; system_config.times_low_voltage+=1;}
+		if(prev_power_mode==MODE_OVERTEMP)	 {system_config.seconds_overtemp+=seconds;    system_config.times_overtemp+=1;   }
+		if(prev_power_mode==MODE_LOW)		 {system_config.seconds_low+=seconds;         system_config.times_low+=1;        }
+		if(prev_power_mode==MODE_HALF)		 {system_config.seconds_med+=seconds;         system_config.times_med+=1;        }
+		if(prev_power_mode==MODE_FULL)		 {system_config.seconds_high+=seconds;        system_config.times_high+=1;       }
+		prev_power_mode=power_mode;
+		if(power_mode==MODE_LOW_VOLTAGE||power_mode==MODE_OFF){
+			//will store data only on power off or low batt to save eeprom
+			eeprom_write_block(&system_config, &nv_system_config, sizeof(system_config));
+		}
+		seconds=0;
+	}
+	
 	return 0;
 }
 
@@ -157,12 +194,6 @@ int main(void){
 	DDRB  = 0b0001;
 	PORTB  = 0b0111;
 
-	all_off();
-	
-	RED_ON;
-	_delay_ms(500);
-	RED_OFF;
-
 	TCCR0A = (1 << COM0B1) | (1 << WGM00);// | (1 << WGM01);  		// PWM mode
 	TCCR0B = (1 << CS01);							// clock source = CLK/8, start PWM
 
@@ -173,6 +204,18 @@ int main(void){
 	ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0) | (1<<ADIE);	// enable ADC on 1mhz/16 with interrupt
 	ADCSRA |= (1<<ADSC);							//start conversion
 
+
+	all_off();
+
+	RED_ON;
+	DRIVER_ON;
+	OCR0B = 8;
+	_delay_ms(500);
+	while(1){};
+	// all_off();
+
+	eeprom_read_block((void*)&system_config, (const void*)&nv_system_config, sizeof(system_config));
+	prev_power_mode = MODE_OFF;
 	sei();
 
 	while(1){
